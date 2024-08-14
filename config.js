1294b0e3523f5e85054ae962cc710378b9797b8c
hiten2110 <hitensharma215@gmail.com>
2024-08-15 01:10:32 +0530

    config.js

--- config.js
+++ config.js
@@ -34,7 +34,6 @@ module.exports = {
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
-  Prefix: "-"
   errorLog: "", 
 
    // Lavalink Server Details
