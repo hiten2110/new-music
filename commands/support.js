3f5cf4947997abb177abf5505ef0324bf217e88b
hiten2110 <hitensharma215@gmail.com>
2024-08-14 23:40:48 +0530

    Add files via upload

---
+++ commands/support.js
@@ -0,0 +1,70 @@
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: "support",
+  description: "Get support server link",
+  permissions: "0x0000000000000800",
+  options: [],
+  run: async (client, interaction) => {
+    try {
+
+      const supportServerLink = "https://discord.gg/minebeach";
+        const embed = new EmbedBuilder()
+            .setColor('#b300ff')
+            .setAuthor({
+              name: 'Support Server',
+              iconURL: 'https://images-ext-1.discordapp.net/external/7I2GVU3LwPx99ogfUNY8UrmYxByIV6rbSrQ-cHtc9uI/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/847339790872870932/497c0dde1dd73a95fcd5959d93f1c725.png?format=webp&quality=lossless&width=204&height=204', 
+              url: 'https://discord.gg/minebeach'
+          })
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n`)
+            .setImage('https://tenor.com/en-GB/view/cristiano-ronaldo-celebrate-oh-yeah-gif-11515686')
+            .setTimestamp();
+
+      return interaction.reply({ embeds: [embed] });
+    } catch (e) {
+    console.error(e); 
+  }
+  },
+};
+
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
