f317161de53949702629b0ed8a2ac8a2e3bdd1fc
hiten2110 <hitensharma215@gmail.com>
2024-08-20 19:10:02 +0530

    config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["847339790872870932", ""],
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  musicardTheme:"themes7", //Goes from themes1 to themes19
-  activityName: "BEACHERS", // This is bot status Write Anything here 
+  musicardTheme:"themes16", //Goes from themes1 to themes19
+  activityName: "BetterMC Staff", // This is bot status Write Anything here 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/minebeach",
+  SupportServer: "https://discord.gg/better-mc",
   CheckmarkIcon: "https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif",
   MusicIcon:"https://media.discordapp.net/attachments/1230824451990622299/1236664581364125787/music-play.gif",
   embedTimeout: 5,  // Timeout before the button interaction embeds are deleted ( Default - 5 seconds)
