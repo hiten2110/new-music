e7f1cf5042d8a07a37fc3a0dbfc2c2fb6137b891
hiten2110 <hitensharma215@gmail.com>
2024-08-23 23:37:54 +0530

    help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,13 @@
 /*
 
-
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by HitenXD!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/bettermc                     ║
+║  ## YouTube : https://www.youtube.com/@HitenXD                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
 
 
 */
